34f97acdfd46e53d510da68738dfac03181c1ab6
badibaew <badibaev44@gmail.com>
2022-09-02 19:15:03 +0600

    C:/Users/user/Desktop/Важное/FrontEnd/HomeWork/Js_part1/3_function

--- 2/app.js
+++ 2/app.js
@@ -6,16 +6,40 @@ const robotsA = [
     {nick : "Moko",damage : 540 },
 ]
 
-const robotsB = [];
+let set = new Array(1,2,1,2,3,9,12,15);
 
-let i = 450;
-
-for (let robotC of robotsA){
-    if(robotC.damage > i){
-        robotsB.push(robotC)
+function getHighNums(arr,percentage,fillpercentage){
+    let perc=0;
+    let sorted=arr.sort(function(a,b){ return a -b});
+    let total=0;
+    for(let i=0;i<arr.length;i++) total+=arr[i];
+    for(let j=sorted.length-1;j>=0;j--){
+        perc+=sorted[j]/total*100;
+        if(fillpercentage){
+            if(perc > percentage) return sorted.slice(j,sorted.length);
+        }else{ 
+            if(sorted[j]/total*100 < percentage) return sorted.slice(j+1,sorted.length);
+        }
     }
+    return sorted;
+
 }
-console.log(robotsB);
+
+console.log(getHighNums(set, 10, false))//9,12,15
+console.log(getHighNums(set, 50, true))
+
+
+
+// const robotsB = [];
+
+// let i = 450;
+
+// for (let robotC of robotsA){
+//     if(robotC.damage > i){
+//         robotsB.push(robotC)
+//     }
+// }
+// console.log(robotsB);
 
 
 
---
+++ 3_function/app.js
@@ -0,0 +1,11 @@
+let question = confirm("Вам есть 18?")
+
+let rull = function(){
+    if(question){
+        console.log("Заходи")
+    }else{
+        console.log("Тебе сюда нельзя")
+    }
+}
+
+rull(); 
